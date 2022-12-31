3bce3e8b5f964a707c0ffe22ae9f81caaff694f5
Peter Green <P.J.Green@bristol.ac.uk>
2022-12-31 17:55:36 +0000

    improved summary

--- R/summary.gRv.R
+++ R/summary.gRv.R
@@ -14,13 +14,18 @@ summary.gRv<-function (object, domain = TRUE, nodes = FALSE, jt = FALSE, print.c
         if (compiled) {
             propagated <- object$net$isPropagated; if(is.null(propagated)) propagated<-FALSE
             evidence.to.propagate<-!is.null(object$net$cache)
+            unprop.evid.nodes<-names(object$net$cache)
+            prop.evid.nodes<-object$net$evi$nodes
         }
         else {
             propagated <- NULL
             evidence.to.propagate<-NULL
+            unprop.evid.nodes<-NULL
+            prop.evid.nodes<-NULL
         }
         domain.summary <- list(compiled = compiled, compressed = compressed, propagated = propagated,
-		evidence.to.propagate=evidence.to.propagate)
+		evidence.to.propagate=evidence.to.propagate,
+            prop.evid.nodes=prop.evid.nodes,unprop.evid.nodes=unprop.evid.nodes)
     }
     jt.summary <- NULL
     if(jt&&compiled) 
@@ -33,11 +38,16 @@ structure(res,class="summary.gRv")
 
 print.summary.gRv<-function(x, ...)
 {
+if(!is.null(x$domain))
+{
 cat('gRaven Domain:\n')
 cat(ifelse(x$domain$compiled,'   is compiled\n','   is not compiled\n'))
 cat(ifelse(x$domain$compressed,'   is compressed\n','   is not compressed\n'))
 cat(ifelse(x$domain$propagated,'   is propagated\n','   is not propagated\n'))
 cat(ifelse(x$domain$evidence.to.propagate,'   evidence','   no evidence'),'has been entered since the last propagation\n')
+if(!is.null(x$domain$prop.evid.nodes)) cat('   propagated evidence on nodes:',x$domain$prop.evid.nodes,'\n')
+if(!is.null(x$domain$unprop.evid.nodes)) cat('   unpropagated evidence on nodes:',x$domain$unprop.evid.nodes,'\n')
+}
 if(!is.null(x$jt)) 
 {
 cat('Junction Tree:\n')
